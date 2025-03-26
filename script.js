6ce3da25428784a9b0e1b8598913ef2208fe6c7c
Yureusu <yureusu9@gmail.com>
2025-03-26 21:11:01 +0800

    Bewitch Created

---
+++ script.js
@@ -0,0 +1,269 @@
+import { renderSelene } from "./selene.js";
+
+export function renderHome(){
+    const topNav = document.createElement("div");
+    topNav.setAttribute("id", "top-nav");
+    const howtoPlay = document.createElement("span");
+    howtoPlay.setAttribute("id", "howto-play");
+    howtoPlay.textContent = "Tutorial";
+    const stats = document.createElement("span");
+    stats.setAttribute("id", "stats");
+    stats.textContent = "Statistics";
+    const coffee = document.createElement("span");
+    coffee.setAttribute("id", "coffee");
+    coffee.textContent = "Donate";
+
+    const tagLineWrapper = document.createElement("div");
+    tagLineWrapper.setAttribute("id", "tagline-wrapper");
+    
+    const tagLine = document.createElement("p");
+    tagLine.setAttribute("id", "tag-line");
+    tagLine.textContent = `"Luck is your guide, strategy is your edge, and magic is your fate."`;
+
+    const gameNav = document.createElement("div");
+    gameNav.setAttribute("id", "game-nav");
+
+    const catalog = document.createElement("img");
+    catalog.setAttribute("id", "catalog");
+    catalog.src = "assets/img/catalog.png";
+
+    const contracts = document.createElement("img");
+    contracts.setAttribute("id", "contracts");
+    contracts.src = "assets/img/contracts.png";
+
+    const shop = document.createElement("img");
+    shop.setAttribute("id", "shop");
+    shop.src = "assets/img/shop.png";
+
+    const gameNavInfo = document.createElement("div");
+    gameNavInfo.setAttribute("id", "gamenav-info");
+    
+    const catalogTitle = document.createElement("p");
+    catalogTitle.setAttribute("id", "catalog-title");
+    catalogTitle.textContent = "Catalog";
+    const contractsTitle = document.createElement("p");
+    contractsTitle.setAttribute("id", "contracts-title");
+    contractsTitle.textContent = "Contracts";
+    const shopTitle = document.createElement("p");
+    shopTitle.setAttribute("id", "shop-title");
+    shopTitle.textContent = "Shop";
+    
+    const introduction = document.createElement("p");
+    introduction.setAttribute("id", "introduction");
+    introduction.textContent = `Oh my, you are cursed by witches, there's no going back now,
+                    you need to fight them one by one my friend, you can open the catalog
+                    to see all the spells you've unlocked so far and get familiarize with them,
+                    you can open the contracts to duel against the witches for you to be able to 
+                    lift all the curse, you can also buy stuff from the shop to help you with 
+                    your duels, that's all for now, goodluck my friend.`;
+    
+    content.appendChild(topNav);
+    topNav.appendChild(howtoPlay);
+    topNav.appendChild(stats);
+    topNav.appendChild(coffee);
+    content.appendChild(tagLineWrapper);
+    tagLineWrapper.appendChild(tagLine);
+    content.appendChild(gameNav);
+    gameNav.appendChild(catalog);
+    gameNav.appendChild(contracts);
+    gameNav.appendChild(shop);
+    content.appendChild(gameNavInfo);
+    gameNavInfo.appendChild(catalogTitle);
+    gameNavInfo.appendChild(contractsTitle);
+    gameNavInfo.appendChild(shopTitle);
+    content.appendChild(introduction);
+
+    catalog.addEventListener("click", () => {
+        renderCatalog();
+    });
+
+    contracts.addEventListener("click", () => {
+        renderContracts();
+    });
+
+    function renderCatalog(){
+        topNav.removeChild(howtoPlay);
+        topNav.removeChild(stats);
+        topNav.removeChild(coffee);
+        content.removeChild(tagLineWrapper);
+        content.removeChild(gameNav);
+        content.removeChild(gameNavInfo);
+        content.removeChild(introduction);
+    
+        topNav.style.justifyContent = "flex-start";
+    
+        //adding spells
+    
+        const spellImgs = [
+            "assets/spells/amulet.png", "assets/spells/bag.png", "assets/spells/burner.png", "assets/spells/candle (1).png", "assets/spells/candle.png", "assets/spells/cat.png",
+            "assets/spells/cauldron.png", "assets/spells/chiromancy.png", "assets/spells/constellation.png", "assets/spells/crystal.png", "assets/spells/crystal-ball.png", "assets/spells/cup.png",
+            "assets/spells/dragon.png", "assets/spells/eye.png", "assets/spells/fairy.png", "assets/spells/feather.png", "assets/spells/flame.png", "assets/spells/fortune-wheel.png",
+            "assets/spells/hamsa.png", "assets/spells/incense-stick (2).png", "assets/spells/incense-stick.png", "assets/spells/ingredients.png", "assets/spells/magic.png", "assets/spells/magic-box.png",
+            "assets/spells/magic-dust.png", "assets/spells/magic-mirror.png", "assets/spells/magic-ring.png", "assets/spells/magic-wand.png", "assets/spells/moon.png", "assets/spells/mortar.png",
+            "assets/spells/ouija-board.png", "assets/spells/pentacle.png", "assets/spells/potion (1).png", "assets/spells/potion (2).png", "assets/spells/potion.png", "assets/spells/root.png",
+            "assets/spells/rune.png", "assets/spells/scroll.png", "assets/spells/skull.png", "assets/spells/spell.png", "assets/spells/spellbook (2).png", "assets/spells/spellbook.png",
+            "assets/spells/sword.png", "assets/spells/tarot.png", "assets/spells/tasseography.png", "assets/spells/triquetra.png", "assets/spells/unicorn.png", "assets/spells/voodoo-doll.png",
+        ];
+    
+        const spellNames = [
+            "Witch Amulet", "Witch Pouch", "Burner", "Celebration Candle", "Ceremony Candle", "Witch Cat", 
+            "Cauldron", "Chiromancy", "Constellation", "Witch Crystal", "Crystall Ball", "Witch Cup",
+            "Dragon Wing", "Eye of Passage", "Spell Fairy", "Magic Feather", "Demonic Flame", "Fortune Wheel",
+            "Hamsa", "Incense Smoke", "Incense Sticks", "Spell Ingredients", "Witch Table", "Witch Box",
+            "Magic Dust", "Witch Mirror", "Witch Ring", "Magic Wand", "Witching Moon", "Deadman's Mortar",
+            "Ouija Board", "Pentacle", "Witch Fragrance", "Witch Potion", "Bottle o' Elixir", "Witch Root",
+            "Dark Runes", "Witch Contract", "Sacrificial Skull", "Fire Spell", "Magic Book", "Spell Book", 
+            "Witch Dagger", "Magic Tarot", "Tasseography", "Triquetra", "Magic Horn", "Voodoo Doll"
+        ]
+    
+        content.style.height = "auto";
+    
+        const spellWrapper = document.createElement("div");
+        spellWrapper.setAttribute("id", "spell-wrapper");
+    
+        content.appendChild(spellWrapper);
+    
+        spellImgs.forEach((index, item) => {
+            const spellImgWrapper = document.createElement("div");
+            spellImgWrapper.setAttribute("id", "spellimg-wrapper");
+    
+            const spellImg = document.createElement("img");
+            spellImg.setAttribute("id", "spell-img");
+            spellImg.src = index;
+    
+            const spellTitle = document.createElement("p");
+            spellTitle.setAttribute("id", "spell-title");
+            spellTitle.textContent = `${spellNames[item]}`;
+    
+            spellWrapper.appendChild(spellImgWrapper);
+            spellImgWrapper.appendChild(spellImg);
+            spellImgWrapper.appendChild(spellTitle);
+        });
+
+        const catalogBackBtn = document.createElement("i");
+        catalogBackBtn.setAttribute("id", "catalog-backbtn");
+        catalogBackBtn.setAttribute("class", "fa-solid fa-chevron-left");
+    
+        const catalogBackText = document.createElement("p");
+        catalogBackText.setAttribute("id", "catalog-backtext");
+        catalogBackText.textContent = "Go back";
+    
+        topNav.appendChild(catalogBackBtn);
+        topNav.appendChild(catalogBackText);
+    
+        catalogBackBtn.addEventListener("click", () => {
+            content.style.height = "100vh";
+            content.removeChild(spellWrapper);
+            content.removeChild(topNav);
+            renderHome();
+        });
+        catalogBackText.addEventListener("click", () => {
+            content.style.height = "100vh";
+            content.removeChild(spellWrapper);
+            content.removeChild(topNav);
+            renderHome();
+        });
+    }
+    
+    function renderContracts(){
+        topNav.removeChild(howtoPlay);
+        topNav.removeChild(stats);
+        topNav.removeChild(coffee);
+        content.removeChild(tagLineWrapper);
+        content.removeChild(gameNav);
+        content.removeChild(gameNavInfo);
+        content.removeChild(introduction);
+    
+        topNav.style.justifyContent = "flex-start";
+    
+        const contractsBackBtn = document.createElement("i");
+        contractsBackBtn.setAttribute("id", "contracts-backbtn");
+        contractsBackBtn.setAttribute("class", "fa-solid fa-chevron-left");
+    
+        const contractsBackText = document.createElement("p");
+        contractsBackText.setAttribute("id", "contracts-backtext");
+        contractsBackText.textContent = "Go back";
+    
+        topNav.appendChild(contractsBackBtn);
+        topNav.appendChild(contractsBackText);
+    
+        //adding witches
+        const witchImgs = [
+            "assets/contracts/Selene.png", "assets/contracts/Sage.png", "assets/contracts/Willow.png", 
+            "assets/contracts/Lilith.png", "assets/contracts/Aspen.png", "assets/contracts/Demise.png",
+        ];
+
+        const witchesName = [
+            "Selene", "Sage", "Willow", "Lilith", "Aspen", "Demise"
+        ];
+
+        content.style.justifyContent = "flex-start";
+
+        const witchWrapper = document.createElement("div");
+        witchWrapper.setAttribute("id", "witch-wrapper");
+
+        content.appendChild(witchWrapper);
+
+        witchImgs.forEach((index, item) => {
+            const witchImgWrapper = document.createElement("div");
+            witchImgWrapper.setAttribute("id", "witchimg-wrapper");
+
+            const witchImg = document.createElement("img");
+            witchImg.setAttribute("id", "witch-img");
+            witchImg.src = index;
+            witchImg.setAttribute("name", `${witchesName[item]}`);
+
+            const witchName = document.createElement("p");
+            witchName.setAttribute("id", "witch-name");
+            witchName.textContent = `${witchesName[item]}`;
+
+            witchWrapper.appendChild(witchImgWrapper);
+            witchImgWrapper.appendChild(witchImg);
+            witchImgWrapper.appendChild(witchName);
+
+            witchImg.addEventListener("click", (event) => {
+                const targetWitch = event.target.getAttribute("name");
+                if(targetWitch === "Selene"){
+                    content.removeChild(witchWrapper);
+                    renderSelene();
+                }
+                else if(targetWitch === "Sage"){
+                    alert(`You clicked: ${witchesName[item]}`);
+                }
+                else if(targetWitch === "Willow"){
+                    alert(`You clicked: ${witchesName[item]}`);
+                }
+                else if(targetWitch === "Lilith"){
+                    alert(`You clicked: ${witchesName[item]}`);
+                }
+                else if(targetWitch === "Aspen"){
+                    alert(`You clicked: ${witchesName[item]}`);
+                }
+                else if(targetWitch === "Demise"){
+                    alert(`You clicked: ${witchesName[item]}`);
+                }
+                else{
+                    alert(`There's no witch with that name.`);
+                }
+            });
+        });
+    
+        contractsBackBtn.addEventListener("click", () => {
+            content.style.height = "100vh";
+            content.style.justifyContent = "space-around";
+            content.removeChild(witchWrapper);
+            content.removeChild(topNav);
+            renderHome();
+        });
+        contractsBackText.addEventListener("click", () => {
+            content.style.height = "100vh";
+            content.style.justifyContent = "space-around";
+            content.removeChild(witchWrapper);
+            content.removeChild(topNav);
+            renderHome();
+        });
+    }
+    
+}
+
+renderHome();
---
+++ selene.js
@@ -0,0 +1,355 @@
+import { renderHome } from "./script.js";
+
+export function renderSelene(){
+    renderHome();
+
+    const content = document.getElementById("content");
+    content.style.height = "auto";
+    content.innerHTML = "";
+
+    const topNav = document.createElement("div");
+    topNav.setAttribute("id", "top-nav");
+    topNav.style.justifyContent = "flex-start";
+
+    const catalogBackBtn = document.createElement("i");
+    catalogBackBtn.setAttribute("id", "catalog-backbtn");
+    catalogBackBtn.setAttribute("class", "fa-solid fa-chevron-left");
+
+    const catalogBackText = document.createElement("p");
+    catalogBackText.setAttribute("id", "catalog-backtext");
+    catalogBackText.textContent = "Go back";
+
+    content.appendChild(topNav);
+    topNav.appendChild(catalogBackBtn);
+    topNav.appendChild(catalogBackText);
+
+    //Selene
+
+    const contractWrapper = document.createElement("div");
+    contractWrapper.setAttribute("id", "contract-wrapper");
+
+    const contractImg = document.createElement("img");
+    contractImg.setAttribute("id", "contract-img");
+    contractImg.src = "assets/contracts/Selene.png";
+
+    const contractInfoWrapper = document.createElement("div");
+    contractInfoWrapper.setAttribute("id", "contractinfo-wrapper");
+
+    const contractName = document.createElement("p");
+    contractName.setAttribute("id", "contract-name");
+    contractName.textContent = `Selene`;
+
+    const contractInfo = document.createElement("p");
+    contractInfo.setAttribute("id", "contract-info");
+    contractInfo.textContent = `Selene seeks to become the greatest voodoo witch in the entire magic world, 
+                she has a deep connection to the spirit world and often serves as an intermediary between 
+                the living and the spirits. She believes that mastering the art of voodoo is the key to true wisdom.`;
+
+    const duelBtn = document.createElement("span");
+    duelBtn.setAttribute("id", "duel-btn");
+    duelBtn.textContent = "Duel";
+
+    const contractCardsWrapper = document.createElement("div");
+    contractCardsWrapper.setAttribute("id", "contractcards-wrapper");
+
+    const contractCardsTitle = document.createElement("p");
+    contractCardsTitle.setAttribute("id", "contractcards-title");
+    contractCardsTitle.textContent = "Selene Cards: ";
+
+    content.appendChild(contractWrapper);
+    contractWrapper.appendChild(contractImg);
+    contractWrapper.appendChild(contractInfoWrapper);
+    contractInfoWrapper.appendChild(contractName);
+    contractInfoWrapper.appendChild(contractInfo);
+    contractInfoWrapper.appendChild(duelBtn);
+    content.appendChild(contractCardsWrapper);
+    contractCardsWrapper.appendChild(contractCardsTitle);
+
+    const contractCards = [
+        "assets/cards/Selene/elixir.png", "assets/cards/Selene/voodoo.png", 
+        "assets/cards/Selene/dagger.png", "assets/cards/Selene/passage.png",
+    ];
+
+    const contractCardsImgWrapper = document.createElement("div");
+    contractCardsImgWrapper.setAttribute("id", "contractcardsimg-wrapper");
+
+    contractCardsWrapper.appendChild(contractCardsImgWrapper);
+
+    contractCards.forEach((index) => {
+        const contractCardImg = document.createElement("img");
+        contractCardImg.setAttribute("id", "contractcards-img");
+        contractCardImg.src = index;
+        contractCardsImgWrapper.appendChild(contractCardImg);
+    });
+
+    duelBtn.addEventListener("click", () => {
+        alert("Duel Selene");
+        duelSelene();
+    });
+
+    catalogBackBtn.addEventListener("click", () => {
+        content.style.height = "100vh";
+        content.style.justifyContent = "space-around";
+        content.removeChild(contractWrapper);
+        content.removeChild(contractCardsWrapper);
+        content.removeChild(topNav);
+        renderHome();
+    });
+    catalogBackText.addEventListener("click", () => {
+        content.style.height = "100vh";
+        content.style.justifyContent = "space-around";
+        content.removeChild(contractWrapper);
+        content.removeChild(contractCardsWrapper);
+        content.removeChild(topNav);
+        renderHome();
+    });
+}
+
+const player = {
+    health: 10,
+    round: 1,
+    prompt: "Open a card. Finish 10 rounds to beat Selene."
+}
+
+function duelSelene(){
+    content.style.height = "100vh";
+    const contractWrapper = document.getElementById("contract-wrapper");
+    content.removeChild(contractWrapper);
+    const contractCardsWrapper = document.getElementById("contractcards-wrapper");
+    content.removeChild(contractCardsWrapper);
+    const topNav = document.getElementById("top-nav");
+    content.removeChild(topNav);
+    console.log(`Remaining health: ${player.health}, Round: ${player.round}`);
+
+    //playfield
+
+    const playField = document.createElement("div");
+    playField.setAttribute("id", "play-field");
+
+    const cardField = document.createElement("div");
+    cardField.setAttribute("id", "card-field");
+
+    const playerInfo = document.createElement("div");
+    playerInfo.setAttribute("id", "player-info");
+
+    content.appendChild(playField);
+    playField.appendChild(cardField);
+    playField.appendChild(playerInfo);
+
+    const cardsArr = [
+        "assets/cards/Selene/elixir.png", "assets/cards/Selene/voodoo.png", 
+        "assets/cards/Selene/dagger.png", "assets/cards/Selene/passage.png", 
+    ];
+
+    //display all
+    updateHealth();
+    updateRound();
+    updatePrompt();
+    createCards();
+
+    //create cards
+    function createCards(){
+        const shuffledCards = shuffleArray(cardsArr);
+        shuffledCards.forEach((index) => {
+            const cardWrapper = document.createElement("div");
+            cardWrapper.setAttribute("id", "card-wrapper");
+    
+            const card = document.createElement("div");
+            card.setAttribute("id", "card");
+    
+            const frontFace = document.createElement("div");
+            frontFace.setAttribute("id", "front-face");
+    
+            const backFace = document.createElement("div");
+            backFace.setAttribute("id", "back-face");
+            const backFaceImg = document.createElement("img");
+            backFaceImg.setAttribute("id", "backface-img");
+            backFaceImg.src = "assets/cards/card-back.png";
+            backFaceImg.setAttribute("name", index);
+    
+            cardField.appendChild(cardWrapper);
+            cardWrapper.appendChild(card);
+            card.appendChild(frontFace);
+            card.appendChild(backFace);
+            
+            backFace.appendChild(backFaceImg);
+    
+            backFaceImg.addEventListener("click", (event) => {
+                const targetCard = event.target.getAttribute("name");
+                card.style.transform = "rotateY(180deg)";
+    
+                if(targetCard === "assets/cards/Selene/elixir.png"){
+                    const frontFaceImg = document.createElement("img");
+                    frontFaceImg.setAttribute("id", "frontface-img");
+                    frontFaceImg.src = index;
+                    frontFace.appendChild(frontFaceImg);  
+                     
+                    const healthWrapper = document.getElementById("health-wrapper");
+                    playerInfo.removeChild(healthWrapper);
+                    const roundWrapper = document.getElementById("round-wrapper");
+                    playerInfo.removeChild(roundWrapper);    
+                    const promptWrapper = document.getElementById("prompt-wrapper");
+                    playerInfo.removeChild(promptWrapper);
+
+                    player.health += 1;
+                    updateHealth();
+                    updateRound();
+                    player.prompt = "Bottle o' Elixir: Gain 1 heart.";
+                    updatePrompt();
+                }
+                else if(targetCard === "assets/cards/Selene/voodoo.png"){
+                    const frontFaceImg = document.createElement("img");
+                    frontFaceImg.setAttribute("id", "frontface-img");
+                    frontFaceImg.src = index;
+                    frontFace.appendChild(frontFaceImg); 
+                  
+                    const healthWrapper = document.getElementById("health-wrapper");
+                    playerInfo.removeChild(healthWrapper);    
+                    const roundWrapper = document.getElementById("round-wrapper");
+                    playerInfo.removeChild(roundWrapper);       
+                    const promptWrapper = document.getElementById("prompt-wrapper");
+                    playerInfo.removeChild(promptWrapper);
+
+                    player.health -= 1;
+                    updateHealth();
+                    updateRound();
+                    player.prompt = "Voodoo Doll: Lose 1 heart.";
+                    updatePrompt();
+                }
+                else if(targetCard === "assets/cards/Selene/dagger.png"){
+                    const frontFaceImg = document.createElement("img");
+                    frontFaceImg.setAttribute("id", "frontface-img");
+                    frontFaceImg.src = index;
+                    frontFace.appendChild(frontFaceImg); 
+      
+                    const healthWrapper = document.getElementById("health-wrapper");
+                    playerInfo.removeChild(healthWrapper)  
+                    const roundWrapper = document.getElementById("round-wrapper");
+                    playerInfo.removeChild(roundWrapper);
+                    const promptWrapper = document.getElementById("prompt-wrapper");
+                    playerInfo.removeChild(promptWrapper);
+
+                    player.health -= 2;
+                    updateHealth();
+                    updateRound();
+                    player.prompt = "Witch Dagger: Lose 2 hearts.";
+                    updatePrompt();
+                }
+                else if(targetCard === "assets/cards/Selene/passage.png"){
+                    const frontFaceImg = document.createElement("img");
+                    frontFaceImg.setAttribute("id", "frontface-img");
+                    frontFaceImg.src = index;
+                    frontFace.appendChild(frontFaceImg); 
+
+                    const healthWrapper = document.getElementById("health-wrapper");
+                    playerInfo.removeChild(healthWrapper);
+                    const roundWrapper = document.getElementById("round-wrapper");
+                    playerInfo.removeChild(roundWrapper);
+                    const promptWrapper = document.getElementById("prompt-wrapper");
+                    playerInfo.removeChild(promptWrapper);
+
+                    updateHealth();
+                    player.round += 1;
+                    updateRound();
+                    player.prompt = "Eye o' Passage: Proceed to next round.";
+                    updatePrompt();
+                    
+                    setTimeout(() => {
+                        cardsArr.forEach(() => {
+                            const removeAll = document.getElementById("card-wrapper");
+                            removeAll.remove();
+                        });
+                    }, 2000); 
+                    
+                    setTimeout(() => {
+                        createCards();
+                    }, 2000);   
+                }
+            });
+        });
+    }
+}
+
+function updateHealth(){
+    const playerInfo = document.getElementById("player-info");
+
+    const healthWrapper = document.createElement("div");
+    healthWrapper.setAttribute("id", "health-wrapper");
+
+    const healthImg = document.createElement("img");
+    healthImg.setAttribute("id", "health-img");
+    healthImg.src = "assets/img/heart.png";
+
+    const displayHealth = document.createElement("p");
+    displayHealth.setAttribute("id", "health-display");
+    displayHealth.textContent = player.health;
+
+    playerInfo.appendChild(healthWrapper);
+    healthWrapper.appendChild(healthImg);
+    healthWrapper.appendChild(displayHealth);
+
+    if(player.health <= 0){
+        alert("You have been defeated.");
+        setTimeout(() => {
+            const content = document.getElementById("content");
+            content.style.height = "100vh";
+            content.style.justifyContent = "space-around";
+            const playField = document.getElementById("play-field");
+            content.removeChild(playField);
+            renderHome();
+        }, 2000);  
+    }
+}
+
+function updateRound(){
+    const playerInfo = document.getElementById("player-info");
+
+    const roundWrapper = document.createElement("div");
+    roundWrapper.setAttribute("id", "round-wrapper");
+
+    const roundImg = document.createElement("img");
+    roundImg.setAttribute("id", "round-img");
+    roundImg.src = "assets/img/round.png";
+
+    const displayRound = document.createElement("p");
+    displayRound.setAttribute("id", "round-display");
+    displayRound.textContent = player.round;
+
+    playerInfo.appendChild(roundWrapper);
+    roundWrapper.appendChild(roundImg);
+    roundWrapper.appendChild(displayRound);
+
+    if(player.round == 11){
+        alert("Selena has been defeated. You won!");
+        setTimeout(() => {
+            const content = document.getElementById("content");
+            content.style.height = "100vh";
+            content.style.justifyContent = "space-around";
+            const playField = document.getElementById("play-field");
+            content.removeChild(playField);
+            renderHome();
+        }, 2000); 
+    }
+}
+
+function updatePrompt(){
+    const playerInfo = document.getElementById("player-info");
+
+    const promptWrapper = document.createElement("div");
+    promptWrapper.setAttribute("id", "prompt-wrapper");
+
+    const promptText = document.createElement("p");
+    promptText.setAttribute("id", "prompt-text");
+    promptText.textContent = player.prompt;
+
+    playerInfo.appendChild(promptWrapper);
+    promptWrapper.appendChild(promptText);
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    }
+    return array;
+}
